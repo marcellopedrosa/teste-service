582be7bc5c3065dd991ce8cd780595109132db28
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-06-16 15:52:59 -0300

    Add files via upload

---
+++ src/main/java/br/com/csc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.csc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Olá do teste-service!";
+    }
+}
