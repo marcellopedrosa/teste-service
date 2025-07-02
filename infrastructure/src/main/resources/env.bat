816218b323b0f12cc4e6982ce05d0e143a8a2172
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-07-02 10:19:01 -0300

    Update env.bat

--- infrastructure/src/main/resources/env.bat
+++ infrastructure/src/main/resources/env.bat
@@ -5,10 +5,10 @@ setx DB_TESTE_SERVICE_URL "jdbc:postgresql://host_do_docker:5444/test_db" /M
 setx DB_TESTE_SERVICE_USERNAME "user_db" /M
 setx DB_TESTE_SERVICE_PASSWORD "pass_db" /M
 
-setx KEYCLOAK_REALM "meu-realm" /M
-setx KEYCLOAK_TESTE_SERVICE_CLIENT_ID "teste-service" /M
-setx KEYCLOAK_TESTE_SERVICE_CLIENT_SECRET "01n2ug8OELqcP77NH0KoLzSCMc06OkwG" /M
-setx KEYCLOAK_URI_SERVER "http://localhost.com.br/auth/realms/meu-realm" /M
+setx SPI_REALM "meu-realm" /M
+setx SPI_TESTE_SERVICE_CLIENT_ID "teste-service" /M
+setx SPI_TESTE_SERVICE_CLIENT_SECRET "01n2ug8OELqcP77NH0KoLzSCMc06OkwG" /M
+setx SPI_URI_SERVER "http://localhost.com.br/auth/realms/meu-realm" /M
 
 setx SPRING_CLOUD_EUREKA_HOST "http://localhost" /M
 setx SPRING_CLOUD_EUREKA_PORT "8761" /M
